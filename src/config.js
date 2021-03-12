089f14817998b3aef93143a8f92434daa5382a65
kk <41568217+w1790226729@users.noreply.github.com>
2021-03-12 16:38:39 +0800

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+ // baseURL: 'http://39.108.3.12:3000',  //后端地址
+ // git accesstoken : e8000377e1bec39ee140746d96c92c209f9b5ad3
+  baseURL: 'http://127.0.0.1:3000',
+  domain: 'http://qp9uzooik.hn-bkt.clouddn.com/',        //七牛云存储地址
+}
